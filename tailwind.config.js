90cd8c1c353d3712ef20f2484f01ac0a163a836d
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-31 14:06:30 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
     darkMode: "class",
     content: ["./*.{html,js}"],
-    theme: {},
+    theme: {
+        colors: {
+            'myBlue': '#386A8E',
+            'white': '#fff'
+        },
+    },
     plugins: []
 }
