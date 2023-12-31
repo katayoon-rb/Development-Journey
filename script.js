0b5b31cb21da47cc20c3d3d5d3a2957d8e06b7a6
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-31 14:06:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -335,12 +335,12 @@ courses.forEach(course => {
     buttons = ``
     Object.keys(links).forEach(x => {
         buttons += `
-            <a href=${links[x]} class="w-52">
+            <a href=${links[x]} class="w-52 text-white">
                 <button class="inline-flex items-center justify-center mt-2 px-4 py-2
                         rounded-md text-sm font-medium ring-offset-background transition-colors
                         focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
                         focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
-                        bg-cyan-100 h-10 w-full">
+                        bg-myBlue h-10 w-full text-white font-semibold">
                     ${x}</button>
             </a>
         `
