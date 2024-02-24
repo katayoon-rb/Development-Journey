7582e1065e59ff1765288ebf26f8bfcdceaffd23
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-02-24 14:26:52 +0330

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const courses = [
         'Title': 'React Testing Library and Jest',
         'Info': 'A complete, all-in-one guide to fully testing your React projects using React Testing Library and Jest.',
         'Links': {
-            'My Implementation': 'https://github.com/katayoon-rb/React-Testing-Library-and-Jest',
+            // 'My Implementation': 'https://github.com/katayoon-rb/React-Testing-Library-and-Jest',
             'Original Course': 'https://www.udemy.com/course/react-testing-library-and-jest',
         },
         'End': '',
